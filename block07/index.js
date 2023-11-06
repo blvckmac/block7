25a6f7c176803cb7c2edaeb0a135d11e98c775d9
Mac McGee <macmcgee@Macs-Air.attlocal.net>
2023-11-06 16:01:58 -0600

    message here

---
+++ block07/index.js
@@ -0,0 +1,28 @@
+/*
+1. Create an html file to link with the javascript file
+2. link .js file in the html head tag
+3. Declar variables (combo1, combo2, comb3)
+4. Use arithmetic operators when declaring the value of each 
+5. Run each variable to make sure the amount to the value that they need. 
+6. Create a variable for the string
+7. Create alert()
+*/
+// declaring variables
+
+console.log( 7 + 3);
+console.log(8 * 5);
+console.log(29 + 20 - 10);
+
+/* checking that the math is correct
+console.log( 7+ 3)
+console.log(8 * 5)
+console.log(29 + 20 - 10) */
+
+//creating string variable
+const alertsr = "You have received this message because you have been chosen to open an important vault. Here is the secret combination: ";
+
+//Using alert()
+alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination: ");
+/* other way of coding the alert()
+alert(alertStr + combo1 + "_" + combo2 + "_" + combo3);
+*/
